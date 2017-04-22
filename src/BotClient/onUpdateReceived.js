cdf6be8300cc95a7774399db2ed08895847e590b
Pavel Tereschenko <me@pavlik.pro>
2017-04-23 02:01:44 +0300

    Bind inlineButtonPressed event handler to updateReceived event

--- src/BotClient/onUpdateReceived.js
+++ src/BotClient/onUpdateReceived.js
@@ -2,14 +2,20 @@
 
 import {
   getBotCommands,
+  getCallbackQuery,
 } from './selectors';
 
 import onCommandReceived from './onCommandReceived';
+import onInlineButtonPressed from './onInlineButtonPressed';
+
 import type { Update } from '../types';
 
 function onUpdateReceived(update: Update) {
   getBotCommands(update)
     .forEach(onCommandReceived(this, update));
+
+  const callbackQuery = getCallbackQuery(update);
+  if (callbackQuery) onInlineButtonPressed(this, update, callbackQuery);
 }
 
 export default onUpdateReceived;

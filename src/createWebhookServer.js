8cef98b5f23068fa9ab5acce25d21b3110ec035b
Pavel <me@pavlik.pro>
2017-09-27 00:34:42 +0300

    Refactoring webhook server constructor

--- src/createWebhookServer.js
+++ src/createWebhookServer.js
@@ -1,61 +1,20 @@
 /* @flow */
 
-import http from 'http'
-import { resolve } from 'url'
+import http, { Server } from 'http'
 
 import type { UpdateHandler } from './types'
-import type { Result, Update } from './generatedTypes'
 
 import HTTPError from './HTTPError'
+import setWebhookToServer from './setWebhookToServer'
 
 export default (
   bot: UpdateHandler,
-  path: string = '',
+  path: string,
   onError?: (error: HTTPError) => any,
-) => {
+): Server => {
   const server = http.createServer()
-  const url = resolve('/', path)
 
-  server.on(
-    'request',
-    (req: http.IncomingMessage, res: http.ServerResponse) => {
-      try {
-        if (req.method !== 'POST') {
-          throw new HTTPError(
-            req.url === url ? 405 : 501,
-            `Invalid ${req.method} request to ${req.url}`,
-          )
-        }
-
-        if (req.url !== url) {
-          throw new HTTPError(404, `Invalid ${req.method} request to ${req.url}`)
-        }
-
-        let data = ''
-
-        req.on('data', newData => (data += newData || ''))
-
-        req.on('end', () => {
-          try {
-            const request: Result<Update[]> = JSON.parse(data)
-            if (request.ok) request.result.forEach(bot.emit)
-          } finally {
-            res.end()
-          }
-        })
-      } catch (e) {
-        const error = HTTPError.fromCatch(e)
-
-        res.statusCode = error.httpStatusCode
-        res.statusMessage = error.httpStatusMessage
-        res.end()
-
-        onError && onError(
-          HTTPError.fromCatch(e),
-        )
-      }
-    },
-  )
+  setWebhookToServer(bot, path, onError)(server)
 
   return server
 }
---
+++ src/setWebhookToServer.js
@@ -0,0 +1,61 @@
+/* @flow */
+
+import http, { Server } from 'http'
+import { resolve } from 'url'
+
+import type { UpdateHandler } from './types'
+import type { Result, Update } from './generatedTypes'
+
+import HTTPError from './HTTPError'
+
+interface HTTPServer extends Server {}
+
+export default (
+  bot: UpdateHandler,
+  path: string = '',
+  onError?: (error: HTTPError) => any,
+) =>
+  (server: HTTPServer): void => {
+    const url = resolve('/', path)
+
+    server.on(
+      'request',
+      (req: http.IncomingMessage, res: http.ServerResponse) => {
+        try {
+          if (req.method !== 'POST') {
+            throw new HTTPError(
+              req.url === url ? 405 : 501,
+              `Invalid ${req.method} request to ${req.url}`,
+            )
+          }
+
+          if (req.url !== url) {
+            throw new HTTPError(404, `Invalid ${req.method} request to ${req.url}`)
+          }
+
+          let data = ''
+
+          req.on('data', newData => (data += newData || ''))
+
+          req.on('end', () => {
+            try {
+              const request: Result<Update[]> = JSON.parse(data)
+              if (request.ok) request.result.forEach(bot.emit)
+            } finally {
+              res.end()
+            }
+          })
+        } catch (e) {
+          const error = HTTPError.fromCatch(e)
+
+          res.statusCode = error.httpStatusCode
+          res.statusMessage = error.httpStatusMessage
+          res.end()
+
+          onError && onError(
+            HTTPError.fromCatch(e),
+          )
+        }
+      },
+    )
+  }

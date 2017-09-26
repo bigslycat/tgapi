ea11bc34d7662d2927f4b74fa24a85cd38ae4439
Pavel <me@pavlik.pro>
2017-09-27 01:09:58 +0300

    Refactoring HTTPError

--- src/types.js
+++ src/types.js
@@ -167,3 +167,31 @@ export type Spec = (
   UnionSpec |
   UnknownSpec
 )
+
+export type HttpStatusCode = (
+  100 | 101 | 102 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 226 | 300 | 301 | 302 | 303 |
+  304 | 305 | 306 | 307 | 308 | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 |
+  411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 422 | 423 | 424 | 426 | 428 | 429 | 431 | 434 |
+  449 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 509 | 510 | 511 | 520 | 521 | 522 |
+  523 | 524 | 525 | 526
+)
+
+export type HTTPStatusMessage = (
+  'Continue' | 'Switching Protocols' | 'Processing' | 'OK' | 'Created' | 'Accepted' | 'Non' |
+  'No Content' | 'Reset Content' | 'Partial Content' | 'Multi' | 'IM Used' | 'Multiple Choices' |
+  'Moved Permanently' | 'Found' | 'See Other' | 'Not Modified' | 'Use Proxy' |
+  'RESERVED in RFC 2616' | 'Temporary Redirect' | 'Permanent Redirect' | 'Bad Request' |
+  'Unauthorized' | 'Payment Required' | 'Forbidden' | 'Not Found' | 'Method Not Allowed' |
+  'Not Acceptable' | 'Proxy Authentication Required' | 'Request Timeout' | 'Conflict' | 'Gone' |
+  'Length Required' | 'Precondition Failed' | 'Payload Too Large' | 'URI Too Long' |
+  'Unsupported Media Type' | 'Range Not Satisfiable' | 'Expectation Failed' | 'I’m a teapot' |
+  'Unprocessable Entity' | 'Locked' | 'Failed Dependency' | 'Upgrade Required' |
+  'Precondition Required' | 'Too Many Requests' | 'Request Header Fields Too Large' |
+  'Requested host unavailable' | 'Retry With' | 'Unavailable For Legal Reasons' |
+  'Internal Server Error' | 'Not Implemented' | 'Bad Gateway' | 'Service Unavailable' |
+  'Gateway Timeout' | 'HTTP Version Not Supported' | 'Variant Also Negotiates' |
+  'Insufficient Storage' | 'Bandwidth Limit Exceeded' | 'Not Extended' |
+  'Network Authentication Required' | 'Unknown Error' | 'Web Server Is Down' |
+  'Connection Timed Out' | 'Origin Is Unreachable' | 'A Timeout Occurred' |
+  'SSL Handshake Failed' | 'Invalid SSL Certificate'
+)

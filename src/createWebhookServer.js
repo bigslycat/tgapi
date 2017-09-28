bc41a9f6cbeaa4a870473dc622939cafa65591c4
Pavel <me@pavlik.pro>
2017-09-28 23:51:26 +0300

    Change observer typedef in webhook server constructor

--- src/createWebhookServer.js
+++ src/createWebhookServer.js
@@ -2,19 +2,20 @@
 
 import http, { Server } from 'http'
 
-import type { UpdateHandler } from './types'
+import type { PartialObserver } from './types'
+import type { Update } from './generatedTypes'
 
 import HTTPError from './HTTPError'
 import setWebhookToServer from './setWebhookToServer'
 
 export default (
-  bot: UpdateHandler,
+  bot$: PartialObserver<Update>,
   path: string,
   onError?: (error: HTTPError) => any,
 ): Server => {
   const server = http.createServer()
 
-  setWebhookToServer(bot, path, onError)(server)
+  setWebhookToServer(bot$, path, onError)(server)
 
   return server
 }

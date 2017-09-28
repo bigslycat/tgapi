1c25af03b2608e0ea165b9f3756288c2a30f3fa8
Pavel <me@pavlik.pro>
2017-09-29 00:01:53 +0300

    Refactoring update subscription constructor

--- src/createUpdateSubscription.js
+++ src/createUpdateSubscription.js
@@ -1,24 +1,80 @@
 /* @flow */
 
-import { Observable } from 'rxjs/Observable'
-/* :: import { Subscription } from 'rxjs/Subscription' */
+import { Observable, Subject } from 'rxjs'
+import { last } from 'ramda'
 
 import { Client } from './createBotClient'
 
-import type { UpdateHandler } from './types'
+import type { PartialObserver } from './types'
+import type { Update } from './generatedTypes'
+
+type RequestParams = {
+  timeout?: number,
+  offset?: number,
+  allowed_updates?: string[],
+  limit?: number,
+}
+
+const getRequesrParams =
+  (timeout: number, allowedUpdates: void | string[]) =>
+    (offset: number): RequestParams => (
+      allowedUpdates ?
+        { timeout, offset, allowed_updates: allowedUpdates } :
+        { timeout, offset }
+    )
+
+type Options = {
+  allowedUpdates?: string[],
+  timeout?: number,
+}
 
 export default (
-  { emit }: UpdateHandler,
-  { getUpdates }: Client,
-  interval: number = 1000,
-): Subscription =>
-  Observable
-    .interval(interval)
-    .mapTo(undefined)
-    .map(getUpdates)
-    .subscribe(
-      resPromise => resPromise.then(
-        response => response.ok &&
-          response.result.forEach(emit),
-      ),
+  client: Client,
+  observer$: PartialObserver<Update>,
+  options: Options = {},
+) => {
+  const getParams = getRequesrParams(
+    Math.max(options.timeout || 1, 1),
+    options.allowedUpdates,
+  )
+
+  const subject$: Subject<number> = new Subject()
+
+  const result$ =
+    subject$
+      .startWith(1)
+      .map(getParams)
+      .mergeMap(client.getUpdates)
+      .publish()
+
+  result$.connect()
+
+  const updates$: Observable<Update> =
+    result$
+      .filter(res => res.ok)
+      // $FlowFixMe
+      .pluck('result')
+      .filter((updates: Update[]) => !!updates.length)
+      .mergeMap((updates: Update[]) => Observable.from(updates))
+
+  const offsetSubscription =
+    result$.map((res) => {
+      if (!res.ok) return 1
+      const lastUpdate = last(res.result)
+      return lastUpdate ? lastUpdate.update_id + 1 : 1
+    }).subscribe(
+      offset => subject$.next(offset),
     )
+
+  const updatesSubscription =
+    updates$.subscribe(
+      update => observer$.next(update),
+      err => observer$.error && observer$.error(err),
+      () => observer$.complete && observer$.complete(),
+    )
+
+  return () => {
+    offsetSubscription.unsubscribe()
+    updatesSubscription.unsubscribe()
+  }
+}

387acf0849848b48855ce490bdffc7c254d47be2
Pavel <me@pavlik.pro>
2017-09-28 23:55:16 +0300

    Refactoring update observer

--- src/createUpdateHandler.js
+++ src/createUpdateHandler.js
@@ -1,70 +1,77 @@
 /* @flow */
 
+import { values } from 'ramda'
 import { Subject } from 'rxjs/Subject'
 /* :: import { Observable } from 'rxjs/Observable' */
 
 import type { Update } from './generatedTypes'
 
 import type {
-  TypedUpdate,
   RxUpdateHandler,
   UpdateType,
+  MessageUpdate,
+  EditedMessageUpdate,
+  ChannelPostUpdate,
+  EditedChannelPostUpdate,
+  InlineQueryUpdate,
+  ChosenInlineResultUpdate,
+  CallbackQueryUpdate,
+  ShippingQueryUpdate,
+  PreCheckoutQueryUpdate,
 } from './types'
 
-const filterByProp =
-  (update$: Observable<TypedUpdate>) =>
-    (propName: UpdateType): any =>
-      update$.filter(update => update.type === propName)
+type Filter = (update$: Subject<Update>) => (propName: UpdateType) => any
+
+const filterByProp: Filter =
+  update$ => type =>
+    update$
+      .filter(update => !!update[type])
+      .distinctUntilKeyChanged('update_id')
+      .map(({ update_id, ...update }) => ({
+        update_id, type, [type]: update[type],
+      }))
 
 export default (): RxUpdateHandler => {
   const update$: Subject<Update> = new Subject()
 
-  const typed$: Observable<TypedUpdate> = update$.map(
-    ({
-      update_id,
-      message,
-      edited_message,
-      channel_post,
-      edited_channel_post,
-      inline_query,
-      chosen_inline_result,
-      callback_query,
-      shipping_query,
-      pre_checkout_query,
-    }): TypedUpdate => {
-      /* eslint-disable camelcase */
-      if (message) return { update_id, message, type: 'message' }
-      if (edited_message) return { update_id, edited_message, type: 'edited_message' }
-      if (channel_post) return { update_id, channel_post, type: 'channel_post' }
-      if (edited_channel_post) return { update_id, edited_channel_post, type: 'edited_channel_post' }
-      if (inline_query) return { update_id, inline_query, type: 'inline_query' }
-      if (chosen_inline_result) return { update_id, chosen_inline_result, type: 'chosen_inline_result' }
-      if (callback_query) return { update_id, callback_query, type: 'callback_query' }
-      if (shipping_query) return { update_id, shipping_query, type: 'shipping_query' }
-      if (pre_checkout_query) return { update_id, pre_checkout_query, type: 'pre_checkout_query' }
-      /* eslint-enable camelcase */
+  const filterBy = filterByProp(update$)
 
-      return (
-        undefined: any
-      )
-    },
-  ).filter(Boolean)
+  const message$: Observable<MessageUpdate> = filterBy('message')
+  const editedMessage$: Observable<EditedMessageUpdate> = filterBy('edited_message')
+  const channelPost$: Observable<ChannelPostUpdate> = filterBy('channel_post')
+  const editedChannelPost$: Observable<EditedChannelPostUpdate> = filterBy('edited_channel_post')
+  const inlineQuery$: Observable<InlineQueryUpdate> = filterBy('inline_query')
+  const chosenInlineResult$: Observable<ChosenInlineResultUpdate> = filterBy('chosen_inline_result')
+  const callbackQuery$: Observable<CallbackQueryUpdate> = filterBy('callback_query')
+  const shippingQuery$: Observable<ShippingQueryUpdate> = filterBy('shipping_query')
+  const preCheckoutQuery$: Observable<PreCheckoutQueryUpdate> = filterBy('pre_checkout_query')
 
-  const filterBy = filterByProp(typed$)
+  const streams = {
+    message$,
+    editedMessage$,
+    channelPost$,
+    editedChannelPost$,
+    inlineQuery$,
+    chosenInlineResult$,
+    callbackQuery$,
+    shippingQuery$,
+    preCheckoutQuery$,
+  }
 
   return {
-    emit: (update: Update) => update$.next(update),
+    next: (update: Update) => update$.next(update),
+    error: error => update$.error(error),
+    complete: () => update$.complete(),
 
-    update$: typed$,
+    ...streams,
 
-    message$: filterBy('message'),
-    editedMessage$: filterBy('edited_message'),
-    channelPost$: filterBy('channel_post'),
-    editedChannelPost$: filterBy('edited_channel_post'),
-    inlineQuery$: filterBy('inline_query'),
-    chosenInlineResult$: filterBy('chosen_inline_result'),
-    callbackQuery$: filterBy('callback_query'),
-    shippingQuery$: filterBy('shipping_query'),
-    preCheckoutQuery$: filterBy('pre_checkout_query'),
+    update$: (
+      Observable
+        .merge(...values(streams))
+        .distinctUntilChanged((prev, next) => (
+          prev.update_id === next.update_id &&
+          prev.type === next.type
+        ))
+    ),
   }
 }

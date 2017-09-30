ef0df066ba1e71271313533bb46340a7afd87775
Pavel <me@pavlik.pro>
2017-09-30 23:41:26 +0300

    Add update validation to middlware

--- src/createMiddleware.js
+++ src/createMiddleware.js
@@ -3,14 +3,21 @@
 import bodyParser from 'body-parser'
 
 import type { UpdateObserver } from './types'
+import isValidUpdate from './util/isValidUpdate'
 
-export default (observer$: UpdateObserver) => {
+export default (observer: UpdateObserver) => {
   const parserMiddleware = bodyParser.json()
 
   const botMiddlware =
-    (req: any, res: any) => {
-      observer$.next(req.body)
-      res.end()
+    ({ body }: Object, res: any) => {
+      if (isValidUpdate(body)) {
+        observer.next(body)
+        res.end()
+        return
+      }
+
+      observer.error(new Error('Invalid update'))
+      res.status(400).send('Bad Request')
     }
 
   return (req: any, res: any) => (

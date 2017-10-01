2600b5f8cf47bff1ebd0bbda1a1831b8c76fad8f
Pavel <me@pavlik.pro>
2017-10-01 05:32:56 +0300

    Connect Flow to bundle

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+export { default as createBotClient } from './createBotClient'
+
+export { default as createUpdateSubscription } from './createUpdateSubscription'
+export { default as createWebhookServer } from './createWebhookServer'
+export { default as createMiddleware } from './createMiddleware'
+
+export { default as createUpdateObserver } from './createUpdateObserver'

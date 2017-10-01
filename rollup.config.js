8298cc83aa62efb58fd34cd528c647d8d7d00c24
Pavel <me@pavlik.pro>
2017-10-01 05:30:32 +0300

    Bundle config

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
+import packageJson from './package.json'
+
+const deps =
+  Object.keys(packageJson.dependencies)
+    .concat('http', 'url')
+    .join('|')
+
+const reg = new RegExp(`^(${deps})($|/)`)
+
+const banner = `
+/**
+ * tgapi v${packageJson.version}
+ * ${packageJson.description}
+ */
+`
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'lib/bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      plugins: [
+        'ramda',
+        'transform-object-rest-spread',
+      ],
+      presets: [
+        'flow',
+        ['env', {
+          targets: { node: 6 },
+          modules: false,
+        }],
+      ],
+    }),
+  ],
+  external: id => reg.test(id),
+  banner,
+}

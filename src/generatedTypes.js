76a03e69475eb5917c191dd537535aac98c9ac04
Pavel Tereschenko <me@stupidcat.wtf>
2017-12-18 22:28:01 +0300

    Update API types

--- src/generatedTypes.js
+++ src/generatedTypes.js
@@ -31,7 +31,9 @@ export type Update = {
    * The update‘s unique identifier. Update identifiers start from a certain
    * positive number and increase sequentially. This ID becomes especially handy
    * if you’re using Webhooks, since it allows you to ignore repeated updates or
-   * to restore the correct update sequence, should they get out of order.
+   * to restore the correct update sequence, should they get out of order. If
+   * there are no new updates for at least a week, then identifier of the next
+   * update will be chosen randomly instead of sequentially.
    */
   update_id: number,
   /**
@@ -999,7 +1001,7 @@ export type ChatMember = {
    */
   status: string,
   /**
-   * Optional. Restictred and kicked only. Date when restrictions will be lifted
+   * Optional. Restricted and kicked only. Date when restrictions will be lifted
    * for this user, unix time
    */
   until_date?: number,

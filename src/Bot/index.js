c91d938c0be63eab0f5ef19687cf50d4e106a3d5
Pavel Tereschenko <me@stupidcat.wtf>
2018-11-26 05:26:56 +0500

    Add return types export

--- src/Bot/index.js
+++ src/Bot/index.js
@@ -5,6 +5,7 @@ import type { BotCore } from './generated'
 export * from './Bot'
 export * from './generated/apiTypes'
 export * from './types'
+export * from './returnTypes'
 export type { APIRequest } from './privates'
 
 export type { BotCore }
